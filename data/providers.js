2e8be447dee6d5cc9d552ab54fa935928dbe5ee7
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-04-16 21:13:30 +0700

    Create providers.js

---
+++ data/providers.js
@@ -0,0 +1,44 @@
+window.PROVIDER_LIST = [
+  {
+    key: "telkomsel",
+    name: "Telkomsel",
+    desc: "Kuota Internet Telkomsel",
+    image: "https://via.placeholder.com/100"
+  },
+  {
+    key: "tri",
+    name: "Three",
+    desc: "Kuota Internet Tri",
+    image: "https://via.placeholder.com/100"
+  },
+  {
+    key: "xl",
+    name: "XL",
+    desc: "Kuota Internet XL",
+    image: "https://via.placeholder.com/100"
+  },
+  {
+    key: "axis",
+    name: "Axis",
+    desc: "Kuota Internet Axis",
+    image: "https://via.placeholder.com/100"
+  },
+  {
+    key: "indosat",
+    name: "Indosat",
+    desc: "Kuota Internet Indosat",
+    image: "https://via.placeholder.com/100"
+  },
+  {
+    key: "smartfren",
+    name: "Smartfren",
+    desc: "Kuota Internet Smartfren",
+    image: "https://via.placeholder.com/100"
+  },
+  {
+    key: "byu",
+    name: "By.U",
+    desc: "Kuota Internet By.U",
+    image: "https://via.placeholder.com/100"
+  }
+];
