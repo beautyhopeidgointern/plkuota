1d2918921d3b94c0ed73dc00dfd3a9f0ff9dad7d
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-04-16 21:56:34 +0700

    Update providers.js

--- data/providers.js
+++ data/providers.js
@@ -1,44 +1,20 @@
 window.PROVIDER_LIST = [
   {
-    key: "three",
-    name: "Three",
-    desc: "Kuota internet dan paket data Three",
-    image: "https://via.placeholder.com/100?text=3"
-  },
-  {
     key: "telkomsel",
     name: "Telkomsel",
     desc: "Kuota internet dan paket data Telkomsel",
     image: "https://via.placeholder.com/100?text=T"
   },
   {
+    key: "three",
+    name: "Three",
+    desc: "Kuota internet dan paket data Three",
+    image: "https://via.placeholder.com/100?text=3"
+  },
+  {
     key: "indosat",
     name: "Indosat",
     desc: "Kuota internet dan paket data Indosat",
     image: "https://via.placeholder.com/100?text=I"
-  },
-  {
-    key: "xl",
-    name: "XL",
-    desc: "Kuota internet dan paket data XL",
-    image: "https://via.placeholder.com/100?text=XL"
-  },
-  {
-    key: "axis",
-    name: "Axis",
-    desc: "Kuota internet dan paket data Axis",
-    image: "https://via.placeholder.com/100?text=A"
-  },
-  {
-    key: "smartfren",
-    name: "Smartfren",
-    desc: "Kuota internet dan paket data Smartfren",
-    image: "https://via.placeholder.com/100?text=S"
-  },
-  {
-    key: "byu",
-    name: "By.U",
-    desc: "Kuota internet dan paket data By.U",
-    image: "https://via.placeholder.com/100?text=byU"
   }
 ];
