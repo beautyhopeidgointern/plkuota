26111275d23fadd196d73e8643ba01fb3c385427
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-04-16 21:23:58 +0700

    Update providers.js

--- data/providers.js
+++ data/providers.js
@@ -1,44 +1,44 @@
 window.PROVIDER_LIST = [
   {
+    key: "three",
+    name: "Three",
+    desc: "Kuota internet dan paket data Three",
+    image: "https://via.placeholder.com/100?text=3"
+  },
+  {
     key: "telkomsel",
     name: "Telkomsel",
-    desc: "Kuota Internet Telkomsel",
-    image: "https://via.placeholder.com/100"
-  },
-  {
-    key: "tri",
-    name: "Three",
-    desc: "Kuota Internet Tri",
-    image: "https://via.placeholder.com/100"
-  },
-  {
-    key: "xl",
-    name: "XL",
-    desc: "Kuota Internet XL",
-    image: "https://via.placeholder.com/100"
-  },
-  {
-    key: "axis",
-    name: "Axis",
-    desc: "Kuota Internet Axis",
-    image: "https://via.placeholder.com/100"
+    desc: "Kuota internet dan paket data Telkomsel",
+    image: "https://via.placeholder.com/100?text=T"
   },
   {
     key: "indosat",
     name: "Indosat",
-    desc: "Kuota Internet Indosat",
-    image: "https://via.placeholder.com/100"
+    desc: "Kuota internet dan paket data Indosat",
+    image: "https://via.placeholder.com/100?text=I"
+  },
+  {
+    key: "xl",
+    name: "XL",
+    desc: "Kuota internet dan paket data XL",
+    image: "https://via.placeholder.com/100?text=XL"
+  },
+  {
+    key: "axis",
+    name: "Axis",
+    desc: "Kuota internet dan paket data Axis",
+    image: "https://via.placeholder.com/100?text=A"
   },
   {
     key: "smartfren",
     name: "Smartfren",
-    desc: "Kuota Internet Smartfren",
-    image: "https://via.placeholder.com/100"
+    desc: "Kuota internet dan paket data Smartfren",
+    image: "https://via.placeholder.com/100?text=S"
   },
   {
     key: "byu",
     name: "By.U",
-    desc: "Kuota Internet By.U",
-    image: "https://via.placeholder.com/100"
+    desc: "Kuota internet dan paket data By.U",
+    image: "https://via.placeholder.com/100?text=byU"
   }
 ];
