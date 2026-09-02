8d30a05177093bd4ccf6d14b755d9c7aa6fb7397
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-09-03 01:27:53 +0700

    Update providers.js

--- data/providers.js
+++ data/providers.js
@@ -3,18 +3,30 @@ window.PROVIDER_LIST = [
     key: "telkomsel",
     name: "Telkomsel",
     desc: "Kuota internet dan paket data Telkomsel",
-    image: "https://via.placeholder.com/100?text=T"
+    image: "https://faqs.uwu.ai/assets/images/gallery03/17266cc9_original.jpg?v=67d70808"
   },
   {
     key: "three",
     name: "Three",
     desc: "Kuota internet dan paket data Three",
-    image: "https://via.placeholder.com/100?text=3"
+    image: "https://faqs.uwu.ai/assets/images/gallery03/4ce10787_original.jpg?v=67d70808"
   },
   {
     key: "indosat",
     name: "Indosat",
     desc: "Kuota internet dan paket data Indosat",
-    image: "https://via.placeholder.com/100?text=I"
+    image: "https://faqs.uwu.ai/assets/images/gallery03/e5c22070_original.jpg?v=67d70808"
+  },
+    {
+    key: "axis",
+    name: "Axis",
+    desc: "Kuota internet dan paket data Axis",
+    image: "https://faqs.uwu.ai/assets/images/gallery03/107718b8_original.jpg?v=67d70808"
+  },
+    {
+    key: "xl",
+    name: "XL",
+    desc: "Kuota internet dan paket data XL",
+    image: "https://faqs.uwu.ai/assets/images/gallery03/31b813d1_original.jpg?v=67d70808"
   }
 ];
