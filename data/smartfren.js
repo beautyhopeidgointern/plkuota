c43a98717b290bad8b1fc1f6a09207e483b46fbb
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-09-03 09:23:57 +0700

    Create smartfren.js

---
+++ data/smartfren.js
@@ -0,0 +1,111 @@
+window.PRICE_DATA = {
+  title: "Smartfren",
+  subtitle: "Pilih kategori lalu pilih paket yang kamu butuhkan",
+  contact: "6283197962700",
+
+  formFields: [
+    {
+      type: "text",
+      label: "Nomor Tujuan",
+      placeholder: "Masukkan nomor tujuan"
+    }
+  ],
+
+  categories: [
+    /* ========================
+       SMART KUOTA NONSTOP
+    ======================== */
+    {
+      title: "Smart Kuota Nonstop",
+      items: [
+        { name: "SMART 10GB + NONSTOP 30 HARI", price: "Rp 40.600", description: "Paket SMART 10GB + NONSTOP 30 HARI.\nNONSTOP = Paket kuota tambahan dan bisa di gunakan setelah kuota utama habis dengan kecepatan 256Kbps.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "SMART 20GB + NONSTOP 30 HARI", price: "Rp 61.000", description: "Paket SMART 20GB + NONSTOP 30 HARI.\nNONSTOP = Paket kuota tambahan dan bisa di gunakan setelah kuota utama habis dengan kecepatan 256Kbps.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "SMART 30GB + NONSTOP 30 HARI", price: "Rp 75.200", description: "Paket SMART 30GB + NONSTOP 30 HARI.\nNONSTOP = Paket kuota tambahan dan bisa di gunakan setelah kuota utama habis dengan kecepatan 256Kbps.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "SMART 35GB + NONSTOP 30 HARI", price: "Rp 78.900", description: "Paket SMART 35GB + NONSTOP 30 HARI.\nNONSTOP = Paket kuota tambahan dan bisa di gunakan setelah kuota utama habis dengan kecepatan 256Kbps.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
+      ]
+    },
+
+    /* ========================
+       SMART SUPER KUOTA
+    ======================== */
+    {
+      title: "Smart Super Kuota",
+      items: [
+        { name: "Smart 12GB + APK 10GB + Lokal 2-18GB 30 Hari", price: "Rp 65.100", description: "Paket Smart 12GB + APK 10GB + Lokal 2-18GB 30 Hari.\n● Aplikasi Whatssapp, Line, Discord, Grab, Gojek, Bukalapak, Blibli, Lazada, Dana, Ovo, Link Aja. Dengan besaran ( FUP 340-500 MB / hari )\n● Untuk mengaktifkan kuota lokal harus melakukan klaim terlebih dahulu melalui aplikasi MySmartfren / *123*789#\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart 32GB + APK 15GB + Lokal 12-53GB 30 Hari", price: "Rp 132.100", description: "Paket Smart 32GB + APK 15GB + Lokal 12-53GB 30 Hari.\n● Aplikasi Whatssapp, Line, Discord, Grab, Gojek, Bukalapak, Blibli, Lazada, Dana, Ovo, Link Aja. Dengan besaran ( FUP 340-500 MB / hari )\n● Untuk mengaktifkan kuota lokal harus melakukan klaim terlebih dahulu melalui aplikasi MySmartfren / *123*789#\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart 44GB + APK 15GB + Lokal 12-66GB 30 Hari", price: "Rp 143.500", description: "Paket Smart 44GB + APK 15GB + Lokal 12-66GB 30 Hari.\n● Aplikasi Whatssapp, Line, Discord, Grab, Gojek, Bukalapak, Blibli, Lazada, Dana, Ovo, Link Aja. Dengan besaran ( FUP 340-500 MB / hari )\n● Untuk mengaktifkan kuota lokal harus melakukan klaim terlebih dahulu melalui aplikasi MySmartfren / *123*789#\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
+      ]
+    },
+
+    /* ========================
+       SMART UNLIMITED FUP
+    ======================== */
+    {
+      title: "Smart Unlimited FUP",
+      items: [
+        { name: "Smart Unlimited FUP 1GB 1 Hari", price: "Rp 9.900", description: "Paket Smart Unlimited FUP 1GB 1 Hari.\nBatas Pemakaian Wajar harian 1GB/hari. Jika melebihi 1GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 2GB 1 Hari", price: "Rp 11.500", description: "Paket Smart Unlimited FUP 2GB 1 Hari.\nBatas Pemakaian Wajar harian 2GB/hari. Jika melebihi 2GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 2GB 3 Hari", price: "Rp 12.700", description: "Paket Smart Unlimited FUP 2GB 3 Hari.\nBatas Pemakaian Wajar harian 2GB/hari. Jika melebihi 2GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 1GB 3 Hari", price: "Rp 12.800", description: "Paket Smart Unlimited FUP 1GB 3 Hari.\nBatas Pemakaian Wajar harian 1GB/hari. Jika melebihi 1GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 3GB 1 Hari", price: "Rp 13.500", description: "Paket Smart Unlimited FUP 3GB 1 Hari.\nBatas Pemakaian Wajar harian 3GB/hari. Jika melebihi 3GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 3GB 3 Hari", price: "Rp 15.400", description: "Paket Smart Unlimited FUP 3GB 3 Hari.\nBatas Pemakaian Wajar harian 3GB/hari. Jika melebihi 3GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 5GB 1 Hari", price: "Rp 18.600", description: "Paket Smart Unlimited FUP 5GB 1 Hari.\nBatas Pemakaian Wajar harian 5GB/hari. Jika melebihi 5GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 1GB 7 Hari", price: "Rp 18.900", description: "Paket Smart Unlimited FUP 1GB 7 Hari.\nBatas Pemakaian Wajar harian 1GB/hari. Jika melebihi 1GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 5GB 3 Hari", price: "Rp 20.700", description: "Paket Smart Unlimited FUP 5GB 3 Hari.\nBatas Pemakaian Wajar harian 5GB/hari. Jika melebihi 5GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 2GB 7 Hari", price: "Rp 24.700", description: "Paket Smart Unlimited FUP 2GB 7 Hari.\nBatas Pemakaian Wajar harian 2GB/hari. Jika melebihi 2GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 3GB 7 Hari", price: "Rp 31.200", description: "Paket Smart Unlimited FUP 3GB 7 Hari.\nBatas Pemakaian Wajar harian 3GB/hari. Jika melebihi 3GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 1GB 14 Hari", price: "Rp 33.400", description: "Paket Smart Unlimited FUP 1GB 14 Hari.\nBatas Pemakaian Wajar harian 1GB/hari. Jika melebihi 1GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 5GB 7 Hari", price: "Rp 40.000", description: "Paket Smart Unlimited FUP 5GB 7 Hari.\nBatas Pemakaian Wajar harian 5GB/hari. Jika melebihi 5GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Unlimited FUP 500MB All + FUP 1.5GB Lokal Tapal Kuda 30 Hari", price: "Rp 58.500", description: "Paket Unlimited FUP 500MB All + FUP 1.5GB Lokal Tapal Kuda 30 Hari.\nBatas Pemakaian Wajar harian 500MB/hari. Jika melebihi 500MB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 500MB 28 Hari", price: "Rp 64.900", description: "Paket Smart Unlimited FUP 500MB 28 Hari.\nBatas Pemakaian Wajar harian 500MB/hari. Jika melebihi 500MB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 700MB 28 Hari", price: "Rp 65.700", description: "Paket Smart Unlimited FUP 700MB 28 Hari.\nBatas Pemakaian Wajar harian 700MB/hari. Jika melebihi 700MB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 1GB 28 Hari", price: "Rp 65.900", description: "Paket Smart Unlimited FUP 1GB 28 Hari.\nBatas Pemakaian Wajar harian 1GB/hari. Jika melebihi 1GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 2GB 28 Hari", price: "Rp 90.000", description: "Paket Smart Unlimited FUP 2GB 28 Hari.\nBatas Pemakaian Wajar harian 2GB/hari. Jika melebihi 2GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 3GB 28 Hari", price: "Rp 119.900", description: "Paket Smart Unlimited FUP 3GB 28 Hari.\nBatas Pemakaian Wajar harian 3GB/hari. Jika melebihi 3GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 5GB 28 Hari", price: "Rp 144.300", description: "Paket Smart Unlimited FUP 5GB 28 Hari.\nBatas Pemakaian Wajar harian 5GB/hari. Jika melebihi 5GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Unlimited FUP 4GB 28 Hari", price: "Rp 144.900", description: "Paket Smart Unlimited FUP 4GB 28 Hari.\nBatas Pemakaian Wajar harian 4GB/hari. Jika melebihi 4GB/hari masih bisa dipakai tapi kecepatan akan turun.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
+      ]
+    },
+
+    /* ========================
+       SMART DATA MINI
+    ======================== */
+    {
+      title: "Smart Data Mini",
+      items: [
+        { name: "Smart 1GB All 3 Hari", price: "Rp 7.600", description: "Paket Smart 1GB All 3 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart 4GB All 3 Hari", price: "Rp 10.700", description: "Paket Smart 4GB All 3 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart 2GB All 3 Hari", price: "Rp 11.400", description: "Paket Smart 2GB All 3 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart 1GB All 7 Hari", price: "Rp 11.900", description: "Paket Smart 1GB All 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart 3GB All 5 Hari", price: "Rp 14.800", description: "Paket Smart 3GB All 5 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Data 4GB 5hr", price: "Rp 14.900", description: "Paket Smart Data 4GB 5hr. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart 1,5GB All 7 Hari", price: "Rp 15.900", description: "Paket Smart 1,5GB All 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart 6GB All 7 Hari", price: "Rp 17.400", description: "Paket Smart 6GB All 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Data 10GB 6hr", price: "Rp 21.800", description: "Paket Smart Data 10GB 6hr. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart 4GB + Unli App 14Hari", price: "Rp 22.100", description: "Paket Smart 4GB + Unli App 14Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Data 5GB 14hr", price: "Rp 23.000", description: "Paket Smart Data 5GB 14hr. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Data 17GB 14hr", price: "Rp 33.500", description: "Paket Smart Data 17GB 14hr. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Data 15GB 10hr", price: "Rp 33.800", description: "Paket Smart Data 15GB 10hr. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Data 7GB 30hr", price: "Rp 34.900", description: "Paket Smart Data 7GB 30hr. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Data 40GB 14hr", price: "Rp 65.300", description: "Paket Smart Data 40GB 14hr. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart Data 66GB 28hr", price: "Rp 101.100", description: "Paket Smart Data 66GB 28hr. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
+      ]
+    },
+
+    /* ========================
+       SMARTFREN COMBO DATA
+    ======================== */
+    {
+      title: "Smartfren Combo Data",
+      items: [
+        { name: "Smart 1GB All + 2GB (01-05) + 1GB Chat 3 Hari", price: "Rp 11.800", description: "Paket Smart 1GB All + 2GB (01-05) + 1GB Chat 3 Hari.\nKuota Chat berlaku untuk akses aplikasi LINE, Whatsapp dan BBM.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "SMART 2GB ALL+2GB Malam(01-05)+2GB Chat 7 Hari", price: "Rp 17.000", description: "Paket SMART 2GB ALL+2GB Malam(01-05)+2GB Chat 7 Hari.\nKuota Chat berlaku untuk akses aplikasi LINE, Whatsapp dan BBM.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "SMART 3GB+5GB Malam(01-05)+3GB Chat 30 Hari", price: "Rp 40.300", description: "Paket SMART 3GB+5GB Malam(01-05)+3GB Chat 30 Hari.\nKuota Chat berlaku untuk akses aplikasi LINE, Whatsapp dan BBM.\nProses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart 10GB All 24 Jam + 20GB (01-05) 30 Hari", price: "Rp 71.100", description: "Paket Smart 10GB All 24 Jam + 20GB (01-05) 30 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart 20GB + 40GB (01-05) 30 Hari", price: "Rp 110.800", description: "Paket Smart 20GB + 40GB (01-05) 30 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart 30GB All + 60GB (01-05) 30 Hari", price: "Rp 138.300", description: "Paket Smart 30GB All + 60GB (01-05) 30 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Smart 40GB+80GB Malam (01-05) 30 Hari", price: "Rp 186.200", description: "Paket Smart 40GB+80GB Malam (01-05) 30 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
+      ]
+    }
+  ]
+};
