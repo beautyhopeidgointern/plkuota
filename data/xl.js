b4961484f3a6f50dd5ff629c27dedcb2ce8ded4e
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-09-25 12:32:55 +0700

    Update xl.js

--- data/xl.js
+++ data/xl.js
@@ -13,75 +13,78 @@ window.PRICE_DATA = {
 
   categories: [
     /* ========================
-       XL FLEX HARIAN 🔵
+       XL FLEX HARIAN 🔵 
     ======================== */
     {
       title: "XL Flex Harian 🔵",
       items: [
-        { name: "XL Data Flex Mini 8GB 1Hari", price: "Rp 7.100", description: "3GB National + Bonus kuota sampai dengan 4GB berlaku di wilayah tertentu di Kotamu - Cek: https://surl.lt/rzktrg. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Mini | 10 GB 1 Hari", price: "Rp 10.400", description: "Kuota 10GB All Jaringan, berlaku 1 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Mini | 6GB 2 Hari", price: "Rp 11.800", description: "Kuota 6GB All Jaringan, berlaku 2 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Mini | 4 GB 3 Hari", price: "Rp 12.800", description: "Kuota 4GB All Jaringan, berlaku 3 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Data Flex Mini 8GB 1Hari", price: "Rp 7.400", description: "3GB National + Bonus kuota sampai dengan 4GB berlaku di wilayah tertentu di Kotamu - Cek: https://surl.lt/rzktrg. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Mini | 10 GB 1 Hari", price: "Rp 10.200", description: "Kuota 10GB All Jaringan, berlaku 1 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Mini | 6GB 2 Hari", price: "Rp 11.900", description: "Kuota 6GB All Jaringan, berlaku 2 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "XL Data Flex Mini 15GB 2Hari", price: "Rp 14.400", description: "Kuota 15GB All Jaringan, berlaku 2 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Mini | 6-8 GB 3 Hari", price: "Rp 14.800", description: "Kuota 6-8GB All Jaringan, berlaku 3 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Mini | 4-5 GB 7 Hari", price: "Rp 17.900", description: "Kuota 4-5GB All Jaringan, berlaku 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Mini | 6-8 GB 7 Hari", price: "Rp 19.100", description: "Kuota 6-8GB All Jaringan, berlaku 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Mini | 5 GB 14 Hari", price: "Rp 19.300", description: "Kuota 5GB All Jaringan, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Mini | 4 GB 3 Hari", price: "Rp 13.000", description: "Kuota 4GB All Jaringan, berlaku 3 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Mini | 6-8 GB 3 Hari", price: "Rp 15.100", description: "Kuota 6-8GB All Jaringan, berlaku 3 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "XL Flex Mini | 20 GB 3 Hari", price: "Rp 19.900", description: "Kuota 20GB All Jaringan, berlaku 3 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Mini | 10-14 GB 7 Hari", price: "Rp 24.000", description: "Kuota 10-14GB All Jaringan, berlaku 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Mini | 7 GB 14 Hari", price: "Rp 24.200", description: "Kuota 7GB All Jaringan, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Mini | 8-10 GB 14 Hari", price: "Rp 28.500", description: "Kuota 8-10GB All Jaringan, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Mini | 4-5 GB 7 Hari", price: "Rp 17.900", description: "Kuota 4-5GB All Jaringan, berlaku 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Mini | 6-8 GB 7 Hari", price: "Rp 19.000", description: "Kuota 6-8GB All Jaringan, berlaku 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Mini | 10-14 GB 7 Hari", price: "Rp 24.200", description: "Kuota 10-14GB All Jaringan, berlaku 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "XL Flex Mini | 15-20 GB 7 Hari", price: "Rp 29.300", description: "Kuota 15-20GB All Jaringan, berlaku 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Mini | 17 GB 14 Hari", price: "Rp 37.600", description: "Kuota 17GB All Jaringan, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "XL Flex Mini | 40 GB 7 Hari", price: "Rp 38.900", description: "Kuota 40GB All Jaringan, berlaku 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Mini | 30 GB 14 Hari", price: "Rp 47.800", description: "Kuota 30GB All Jaringan, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Mini | 75 GB 7 Hari", price: "Rp 49.300", description: "Kuota 75GB All Jaringan, berlaku 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Mini | 75 GB 14 Hari", price: "Rp 72.700", description: "Kuota 75GB All Jaringan, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Mini | 150 GB 14 Hari", price: "Rp 97.600", description: "Kuota 150GB All Jaringan, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
+        { name: "XL Flex Mini | 75 GB 7 Hari", price: "Rp 49.000", description: "Kuota 75GB All Jaringan, berlaku 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Mini | 5 GB 14 Hari", price: "Rp 18.800", description: "Kuota 5GB All Jaringan, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Mini | 7 GB 14 Hari", price: "Rp 24.200", description: "Kuota 7GB All Jaringan, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Mini | 8-10 GB 14 Hari", price: "Rp 28.600", description: "Kuota 8-10GB All Jaringan, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Mini | 17 GB 14 Hari", price: "Rp 37.700", description: "Kuota 17GB All Jaringan, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Mini | 30 GB 14 Hari", price: "Rp 47.400", description: "Kuota 30GB All Jaringan, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Mini | 75 GB 14 Hari", price: "Rp 72.000", description: "Kuota 75GB All Jaringan, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Mini | 150 GB 14 Hari", price: "Rp 96.600", description: "Kuota 150GB All Jaringan, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
       ]
     },
 
     /* ========================
-       XL COMBO FLEX 🔵
+       XL COMBO FLEX 🔵 
     ======================== */
     {
       title: "XL Combo Flex 🔵",
       items: [
         { name: "XL Combo Flex S (3.5GB - 5GB ) 21 Hari", price: "Rp 33.400", description: "Total Kuota 3.5GB Hingga 5GB Nasional, Bonus Nelpon 5 Menit, berlaku 21 Hari. Cek wilayah yang dapat kuota lokal XL di https://www.xl.co.id/produk/paket-dan-addon/xtra-combo-flex. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Data Flex S (5GB - 8GB) + All 5 Menit 28 Hari", price: "Rp 34.900", description: "Total Kuota 5GB Hingga 8GB Nasional, Bonus Nelpon 5 Menit, berlaku 28 Hari. Cek wilayah yang dapat kuota lokal XL di https://www.xl.co.id/produk/paket-dan-addon/xtra-combo-flex. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Data Flex S (5GB - 8GB) + All 5 Menit 28 Hari", price: "Rp 33.800", description: "Total Kuota 5GB Hingga 8GB Nasional, Bonus Nelpon 5 Menit, berlaku 28 Hari. Cek wilayah yang dapat kuota lokal XL di https://www.xl.co.id/produk/paket-dan-addon/xtra-combo-flex. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Data Flex Max 7GB 28 Hari", price: "Rp 35.900", description: "Total Kuota 7GB Nasional, Bonus Nelpon 5 Menit, berlaku 28 Hari. Cek wilayah yang dapat kuota lokal XL di https://www.xl.co.id/produk/paket-dan-addon/xtra-combo-flex. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "XL Combo Flex S+ 7GB + Bonus 28 Hari", price: "Rp 38.100", description: "Total Kuota 7GB Nasional, Bonus Nelpon 5 Menit, berlaku 28 Hari. Cek wilayah yang dapat kuota lokal XL di https://www.xl.co.id/produk/paket-dan-addon/xtra-combo-flex. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "XL Combo Flex M (10GB - 15GB) 28 Hari", price: "Rp 50.000", description: "Total Kuota 10GB Hingga 15GB Nasional, Bonus Nelpon 5 Menit, berlaku 28 Hari. Cek wilayah yang dapat kuota lokal XL di https://www.xl.co.id/produk/paket-dan-addon/xtra-combo-flex. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "XL Combo Flex M+ (14GB - 20GB) 28 Hari", price: "Rp 61.400", description: "Total Kuota 14GB Hingga 20GB Nasional, Bonus Nelpon 5 Menit, berlaku 28 Hari. Cek wilayah yang dapat kuota lokal XL di https://www.xl.co.id/produk/paket-dan-addon/xtra-combo-flex. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Data Flex Max 13 - 16GB 28 Hari", price: "Rp 48.800", description: "Total Kuota 13GB Hingga 16GB Nasional, Bonus Nelpon 5 Menit, berlaku 28 Hari. Cek wilayah yang dapat kuota lokal XL di https://www.xl.co.id/produk/paket-dan-addon/xtra-combo-flex. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "XL Combo Flex L 18GB + Lokal (28hr)", price: "Rp 71.300", description: "Total Kuota 18GB Nasional, Bonus Nelpon 5 Menit, berlaku 28 Hari. Cek wilayah yang dapat kuota lokal XL di https://www.xl.co.id/produk/paket-dan-addon/xtra-combo-flex. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "XL Combo Flex L+ (26GB - 32GB) 28 Hari", price: "Rp 80.900", description: "Total Kuota 26GB Hingga 32GB Nasional, Bonus Nelpon 5 Menit, berlaku 28 Hari. Cek wilayah yang dapat kuota lokal XL di https://www.xl.co.id/produk/paket-dan-addon/xtra-combo-flex. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "XL Combo Flex XL (32GB - 50GB) 28 Hari", price: "Rp 101.000", description: "Total Kuota 32GB Hingga 50GB Nasional, Bonus Nelpon 5 Menit, berlaku 28 Hari. Cek wilayah yang dapat kuota lokal XL di https://www.xl.co.id/produk/paket-dan-addon/xtra-combo-flex. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Max 34 - 40 GB 28 Hari", price: "Rp 76.800", description: "Total Kuota 34GB Hingga 40GB Nasional, Bonus Nelpon 5 Menit, berlaku 28 Hari. Cek wilayah yang dapat kuota lokal XL di https://www.xl.co.id/produk/paket-dan-addon/xtra-combo-flex. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "XL Combo Flex XXL (65GB - 75GB) 28 Hari", price: "Rp 140.500", description: "Total Kuota 65GB Hingga 75GB Nasional, Bonus Nelpon 5 Menit, berlaku 28 Hari. Cek wilayah yang dapat kuota lokal XL di https://www.xl.co.id/produk/paket-dan-addon/xtra-combo-flex. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Combo Flex XXXL 120GB + Bonus 28Hr", price: "Rp 167.100", description: "Total Kuota 120GB Nasional, Bonus Nelpon 5 Menit, berlaku 28 Hari. Cek wilayah yang dapat kuota lokal XL di https://www.xl.co.id/produk/paket-dan-addon/xtra-combo-flex. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
+        { name: "XL Combo Flex XXXL 120GB + Bonus 28Hr", price: "Rp 165.100", description: "Total Kuota 120GB Nasional, Bonus Nelpon 5 Menit, berlaku 28 Hari. Cek wilayah yang dapat kuota lokal XL di https://www.xl.co.id/produk/paket-dan-addon/xtra-combo-flex. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
       ]
     },
 
-        /* ========================
-       XL COMBO FLEX MAX 🔵
+    /* ========================
+       XL COMBO FLEX MAX 🔵 
     ======================== */
     {
       title: "XL Combo Flex Max 🔵",
       items: [
-        { name: "XL Flex Max 7GB 28 Hari", price: "Rp 34.300", description: "Kuota utama 7GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Max 10GB 28 Hari", price: "Rp 42.100", description: "Kuota utama 10GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Max 75GB 14 Hari", price: "Rp 72.200", description: "Kuota utama 75GB, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Max 7GB 28 Hari", price: "Rp 34.800", description: "Kuota utama 7GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Max 10GB 28 Hari", price: "Rp 42.200", description: "Kuota utama 10GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "XL Flex Max 15GB 28 Hari", price: "Rp 48.100", description: "Kuota utama 15GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "XL Flex Max 19 - 23 GB 28 Hari", price: "Rp 58.800", description: "Kuota utama 19-23GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Max 31 GB 28 Hari", price: "Rp 67.600", description: "Kuota utama 31GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Max 75GB 14 Hari", price: "Rp 72.200", description: "Kuota utama 75GB, berlaku 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Max 40 GB 28 Hari", price: "Rp 76.700", description: "Kuota utama 40GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Data FlexMax 28GB 28Hari", price: "Rp 67.600", description: "Kuota utama 28GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Max 34 - 40 GB 28 Hari", price: "Rp 76.700", description: "Kuota utama 34-40GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "XL Flex Max 50GB 28 Hari", price: "Rp 86.300", description: "Kuota utama 50GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "XL Flex Max 65GB 28 Hari", price: "Rp 95.900", description: "Kuota utama 65GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Max 100GB 28 Hari", price: "Rp 120.300", description: "Kuota utama 100GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "XL Flex Max 150GB 28 Hari", price: "Rp 145.500", description: "Kuota utama 150GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
+        { name: "XL Flex Max 100GB 28 Hari", price: "Rp 120.600", description: "Kuota utama 100GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "XL Flex Max 150GB 28 Hari", price: "Rp 144.400", description: "Kuota utama 150GB, berlaku 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
       ]
     },
 
-        /* ========================
-       XL DATA PURE 🔵
+    /* ========================
+       XL DATA PURE 🔵 
     ======================== */
     {
       title: "XL Data Pure 🔵",
@@ -94,8 +97,8 @@ window.PRICE_DATA = {
         { name: "4GB 30 Hari All Jaringan", price: "Rp 29.800", description: "Kuota utama 4GB, berlaku 30 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "5GB 30 Hari All Jaringan", price: "Rp 37.200", description: "Kuota utama 5GB, berlaku 30 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "6GB 30 Hari All Jaringan", price: "Rp 48.000", description: "Kuota utama 6GB, berlaku 30 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "8GB 30 Hari All Jaringan", price: "Rp 63.300", description: "Kuota utama 8GB, berlaku 30 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "10GB 30 Hari All Jaringan", price: "Rp 78.500", description: "Kuota utama 10GB, berlaku 30 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
+        { name: "8GB 30 Hari All Jaringan", price: "Rp 62.900", description: "Kuota utama 8GB, berlaku 30 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "10GB 30 Hari All Jaringan", price: "Rp 78.100", description: "Kuota utama 10GB, berlaku 30 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
       ]
     }
   ]
