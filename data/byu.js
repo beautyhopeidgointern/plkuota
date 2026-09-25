76dec72624a29a70daf2074d81ce675c84077dcc
beautyhopeidgointern <beautyhopeid@gmail.com>
2026-09-25 12:09:25 +0700

    Update byu.js

--- data/byu.js
+++ data/byu.js
@@ -18,21 +18,30 @@ window.PRICE_DATA = {
     {
       title: "BYU Data Harian 🔵",
       items: [
-        { name: "by.U Data 3 GB 1 Hari", price: "Rp 8.400", description: "Paket by.U Data 3 GB 1 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "by.U Data 10 GB 1 Hari", price: "Rp 8.700", description: "Paket by.U Data 10 GB 1 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 1GB 1Hari", price: "Rp 8.300", description: "Paket by.U Data 1GB 1Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 2GB 1Hari", price: "Rp 8.400", description: "Paket by.U Data 2GB 1Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 3 GB 1 Hari", price: "Rp 8.000", description: "Paket by.U Data 3 GB 1 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 10 GB 1 Hari", price: "Rp 8.400", description: "Paket by.U Data 10 GB 1 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 6GB 2Hari", price: "Rp 9.000", description: "Paket by.U Data 6GB 2Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 8 GB 2 Hari", price: "Rp 11.200", description: "Paket by.U Data 8 GB 2 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 12 GB 2 Hari", price: "Rp 12.300", description: "Paket by.U Data 12 GB 2 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Kuota ByU 2GB 3 Hari", price: "Rp 13.200", description: "Paket Kuota ByU 2GB 3 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 3GB 3Hari", price: "Rp 13.300", description: "Paket by.U Data 3GB 3Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 4 GB 3 Hari", price: "Rp 13.100", description: "Paket by.U Data 4 GB 3 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 6 GB 3 Hari", price: "Rp 13.100", description: "Paket by.U Data 6 GB 3 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 8 GB 3 Hari", price: "Rp 14.300", description: "Paket by.U Data 8 GB 3 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "by.U Data 7.5 GB 5 Hari", price: "Rp 17.100", description: "Paket by.U Data 7.5 GB 5 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 7.5 GB 5 Hari", price: "Rp 15.800", description: "Paket by.U Data 7.5 GB 5 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 10 GB 5 Hari", price: "Rp 16.200", description: "Paket by.U Data 10 GB 5 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "by.U Data 3 GB 7 Hari", price: "Rp 13.100", description: "Paket by.U Data 3 GB 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "by.U Data 4 GB 7 Hari", price: "Rp 13.500", description: "Paket by.U Data 4 GB 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "by.U Data 6 GB 7 Hari", price: "Rp 16.500", description: "Paket by.U Data 6 GB 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "by.U Data 7.5 GB 7 Hari", price: "Rp 21.200", description: "Paket by.U Data 7.5 GB 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Kuota Byu 13GB 5 Hari", price: "Rp 19.200", description: "Paket Kuota Byu 13GB 5 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 3 GB 7 Hari", price: "Rp 13.400", description: "Paket by.U Data 3 GB 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 4 GB 7 Hari", price: "Rp 13.300", description: "Paket by.U Data 4 GB 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Kuota ByU 5GB 7 Hari", price: "Rp 15.700", description: "Paket Kuota ByU 5GB 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 6 GB 7 Hari", price: "Rp 16.000", description: "Paket by.U Data 6 GB 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 6.5GB 7Hari", price: "Rp 18.500", description: "Paket by.U Data 6.5GB 7Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 7.5 GB 7 Hari", price: "Rp 18.900", description: "Paket by.U Data 7.5 GB 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 8 GB 7 Hari", price: "Rp 19.100", description: "Paket by.U Data 8 GB 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "by.U Data 10 GB 7 Hari", price: "Rp 21.000", description: "Paket by.U Data 10 GB 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 10 GB 7 Hari", price: "Rp 20.700", description: "Paket by.U Data 10 GB 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "Kuota Byu 14GB 7 Hari", price: "Rp 25.400", description: "Paket Kuota Byu 14GB 7 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 7 GB 20 Hari", price: "Rp 38.000", description: "Paket by.U Data 7 GB 20 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
       ]
     },
@@ -53,14 +62,14 @@ window.PRICE_DATA = {
         { name: "by.U Data 9 GB 28 Hari", price: "Rp 44.800", description: "Paket by.U Data 9 GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 10 GB 28 Hari", price: "Rp 45.000", description: "Paket by.U Data 10 GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 11 GB 28 Hari", price: "Rp 52.300", description: "Paket by.U Data 11 GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "by.U Data 12 GB 28 Hari", price: "Rp 62.900", description: "Paket by.U Data 12 GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 12 GB 28 Hari", price: "Rp 53.200", description: "Paket by.U Data 12 GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 14 GB 28 Hari", price: "Rp 67.400", description: "Paket by.U Data 14 GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 20 GB 28 Hari", price: "Rp 74.500", description: "Paket by.U Data 20 GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 23GB 28 Hari", price: "Rp 74.200", description: "Paket by.U Data 23GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 33 GB 28 Hari", price: "Rp 75.100", description: "Paket by.U Data 33 GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "by.U Data 42 GB 28 Hari", price: "Rp 98.400", description: "Paket by.U Data 42 GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 42 GB 28 Hari", price: "Rp 99.000", description: "Paket by.U Data 42 GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 50 GB 28 Hari", price: "Rp 101.000", description: "Paket by.U Data 50 GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "by.U Data 57GB 28 Hari", price: "Rp 97.300", description: "Paket by.U Data 57GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "by.U Data 57GB 28 Hari", price: "Rp 98.500", description: "Paket by.U Data 57GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 65 GB 28 Hari", price: "Rp 99.900", description: "Paket by.U Data 65 GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "by.U Data 75 GB 28 Hari", price: "Rp 153.100", description: "Paket by.U Data 75 GB 28 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
       ]
@@ -72,14 +81,14 @@ window.PRICE_DATA = {
     {
       title: "By. U Data 14 Hari 🔵",
       items: [
-        { name: "By. U Data 1GB 14 Hari", price: "Rp 18.400", description: "Paket By. U Data 1GB 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "By. U Data 2GB 14 Hari", price: "Rp 18.200", description: "Paket By. U Data 2GB 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "By. U Data 3GB 14 Hari", price: "Rp 17.900", description: "Paket By. U Data 3GB 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "By. U Data 1GB 14 Hari", price: "Rp 18.500", description: "Paket By. U Data 1GB 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "By. U Data 2GB 14 Hari", price: "Rp 18.300", description: "Paket By. U Data 2GB 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "By. U Data 3GB 14 Hari", price: "Rp 18.000", description: "Paket By. U Data 3GB 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "By. U Data 4GB 14 Hari", price: "Rp 25.100", description: "Paket By. U Data 4GB 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
         { name: "By. U Data 5GB 14 Hari", price: "Rp 25.500", description: "Paket By. U Data 5GB 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "By. U Data 6GB 14 Hari", price: "Rp 28.800", description: "Paket By. U Data 6GB 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "By. U Data 7GB 14 Hari", price: "Rp 25.200", description: "Paket By. U Data 7GB 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
-        { name: "By. U Data 8GB 14 Hari", price: "Rp 25.400", description: "Paket By. U Data 8GB 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
+        { name: "By. U Data 7GB 14 Hari", price: "Rp 25.500", description: "Paket By. U Data 7GB 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "By. U Data 8GB 14 Hari", price: "Rp 25.100", description: "Paket By. U Data 8GB 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." },
+        { name: "By. U Data 6GB 14 Hari", price: "Rp 28.800", description: "Paket By. U Data 6GB 14 Hari. Proses otomatis 1-15 menit. Mohon pastikan nomor tujuan sudah benar." }
       ]
     }
   ]
